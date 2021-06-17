05294c7d3b94ce0c81cf25e36e2bc7837e725a41
targino99 <56704740+targino99@users.noreply.github.com>
2021-06-17 19:58:40 -0400

    Create PessoaTeste.cs

---
+++ IdadeComMoqTeste/PessoaTeste.cs
@@ -0,0 +1,31 @@
+namespace IdadeComMoqTeste
+{
+    public class PessoaTeste
+    {
+        [Theory]
+        [InlineData]("Max", 23, "é número primo")
+        [InlineData]("Lucas", 30, "Não é número primo")
+        [InlineData]("Maria", 17, "é número primo")
+        
+        public void VerificaIdade(String Nome, int idade)
+        {
+            //Arrange
+            Pessoa pessoa = new(Nome, idade);
+          
+
+            Mock<Pessoa.IVerificaIdade> mock = new Mock<Pessoa.IVerificaIdade>();
+            mock.Setup(m => m.VerificaIdade(pessoa)).Returns("é primo");
+
+
+            //Act
+            Pessoa verifica = new Pessoa();
+
+            var idadeEsperada = mock.Object.VerificaIdade(pessoa);
+            var idade = verifica.VerificaIdade(pessoa);
+
+            //Assert
+            Assert.Equal(idade, idadeEsperada);
+
+        }
+
+    }    }
