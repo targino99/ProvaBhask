5cda888f84e7e1e0e0a8d9f1ed0dd281cfda27fe
targino99 <56704740+targino99@users.noreply.github.com>
2021-06-17 17:28:05 -0400

    Create Exercicio.cs

---
+++ Exercicio.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExBhaskara
+{
+    public class Exercicio
+    {
+        public int Calculo(int a, int b, int c, double d, double raiz)
+        {
+            return (b * b) - (4 * b * c);
+                            Math.Sqrt;
+            return ((-(b)) + raiz) / (2 * a);
+            return ((-(b)) - raiz) / (2 * a);
+        }
+
+    }
+}
